00907191f51768484d11384af2eb188b0f0ec858
Rodrigo Oliveira <rodrigo.oliveira@on-running.com>
2020-12-15 18:29:45 +0100

    Fixes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh -l
-
-AWS_ACCESS_KEY_ID=$2 AWS_SECRET_ACCESS_KEY=$3 AWS_DEFAULT_REGION=$5 aws eks --region $5 update-kubeconfig --name it-production
+aws eks update-kubeconfig --name $CLUSTER_ID
 chmod 600 /root/.kube/config
 
 output=`helm $1`
